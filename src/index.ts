27a028d3a3a703a9d78fea13ac166b226c7c67f4
Mithun Balasubramanian <sendmithunb@gmail.com>
2024-08-22 21:37:06 -0500

    add middleware and secure routes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express, { Request, Response } from 'express';
 import projectRoutes from './routes/projects';
 import workexperienceRoutes from './routes/workexperience';
+import { authenticateToken } from './middleware/auth';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); 
-app.use('/projects', projectRoutes);
-app.use('/workexperience', workexperienceRoutes);
 
+// Public route
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
+
+// Protected routes
+app.use('/projects', authenticateToken, projectRoutes);
+app.use('/workexperience', authenticateToken, workexperienceRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
